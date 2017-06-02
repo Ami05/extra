983e45d7913d3fbf673e7df1899149a48324cea9
Ami05 <sarthakwork05@gmail.com>
2017-06-02 18:58:15 +0530

    added hello world program

---
+++ class.java
@@ -0,0 +1,16 @@
+class use{
+
+int id=10;
+String name = "Sarthak";
+}
+
+class imp{
+
+public static void main(String[] args ){
+
+	use data = new use();
+	System.out.println(data.id);
+	System.out.println(data.name);
+
+	}
+}
