b542678ab3ff31bb624ab4399bc05418b43e847b
Ami05 <sarthakwork05@gmail.com>
2017-07-04 18:52:14 +0530

    added new java file

---
+++ script2.java
@@ -0,0 +1,8 @@
+class imp{
+
+public static void main(String[] args ){
+	System.out.println("-----------------------------------------------------");
+	System.out.println("GIT INTEGRATION WITH JENKINS");
+	System.out.println("-----------------------------------------------------");
+	}
+}
